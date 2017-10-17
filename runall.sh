875bff824c1a12b69e230d77bb797c542331def2
lowery <lowery@fas.harvard.edu>
2017-10-17 16:52:39 -0400

    Fixed dependency issue related to JAXP and Hadoop

--- runall.sh
+++ runall.sh
@@ -3,7 +3,7 @@ mvn clean
 mvn install -Dmaven.test.skip=true
 
 
-java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName src/main/resources/nevpDup.properties -vectorize
-java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName src/main/resources/nevpDup.properties -cluster
-java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName src/main/resources/nevpDup.properties -dumpClusters
-java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName src/main/resources/nevpDup.properties -interpretClusters
+java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName /nevpDup.properties -vectorize
+java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName /nevpDup.properties -cluster
+java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName /nevpDup.properties -dumpClusters
+java -jar target/FP-DuplicateFinding-0.0.1-SNAPSHOT-jar-with-dependencies.jar Run -configFileName /nevpDup.properties -interpretClusters
